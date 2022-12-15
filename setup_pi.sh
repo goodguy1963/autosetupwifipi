72fde7b960ae85644ee7b33bc049da87aaf47e74
goodguy1963 <49859266+goodguy1963@users.noreply.github.com>
2022-12-15 14:14:49 +0100

    Update setup_pi.sh

--- setup_pi.sh
+++ setup_pi.sh
@@ -1,194 +1,192 @@
 #!/bin/bash
-
-#Step 1: Installation of dependencies
-echo "Installing dependencies..."
+#Installation Dependencies
 apt-get install dnsmasq hostapd apache2
 
-#Step 2: Check internet connection
-echo "Checking internet connection..."
-if ping -q -c 1 -W 1 google.com >/dev/null; then
-echo "Internet connection detected. Exiting script."
-exit 0
+#Check for internet connection
+if ! ping -c 1 google.com > /dev/null 2>&1; then
+# Display message on HDMI output
+echo "Connect to the 'SETUP PI - Network' to enter WIFI credentials"
 else
+exit 0
+fi
 
-#Step 3: Set up access point
-echo "No internet connection detected. Setting up access point..."
-hostapd /etc/hostapd/hostapd.conf
-dnsmasq -C /etc/dnsmasq.conf
+#Create access point with hostapd
+cat > /etc/hostapd/hostapd.conf <<EOF
+interface=wlan0
+driver=nl80211
+ssid=SETUP PI
+hw_mode=g
+channel=7
+wmm_enabled=0
+macaddr_acl=0
+auth_algs=1
+ignore_broadcast_ssid=0
+wpa=2
+wpa_passphrase=12345678
+wpa_key_mgmt=WPA-PSK
+rsn_pairwise=CCMP
+EOF
+
+#Configure DHCP server with dnsmasq
+cat > /etc/dnsmasq.conf <<EOF
+interface=wlan0
+dhcp-range=10.0.0.2,10.0.0.5,255.255.255.0,12h
+dhcp-option=3,10.0.0.1
+dhcp-option=6,10.0.0.1
+EOF
+
+#Enable IP forwarding and NAT
+echo 1 > /proc/sys/net/ipv4/ip_forward
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-echo "1" > /proc/sys/net/ipv4/ip_forward
+
+#Start services
 service hostapd start
 service dnsmasq start
 
-#Step 4: Create GUI for wifi credentials input
-echo "Creating GUI for wifi credentials input..."
-cp /var/www/html/index.html /var/www/html/index.html.bak
+#Create web server with GUI for wifi credentials
 cat > /var/www/html/index.html <<EOF
-  <html>
-    <head>
-      <title>SETUP PI Wifi Credentials</title>
-    </head>
-    <body>
-      <h1>Enter your wifi credentials:</h1>
-      <form action="submit.php" method="post">
-        SSID: <input type="text" name="ssid"><br>
-        Password: <input type="password" name="password"><br>
-        Security Type:
-        <input type="radio" name="security" value="wpa" checked>WPA
-        <input type="radio" name="security" value="wep">WEP
-        <input type="radio" name="security" value="wpa2">WPA2
-        <input type="radio" name="security" value="eap">EAP<br>
-        Optional Proxy Configuration:
-        <input type="checkbox" name="proxy" value="1">HTTP/HTTPS Proxy
-        <input type="text" name="proxy_server"><br>
-        Proxy Port: <input type="text" name="proxy_port"><br>
-        Proxy Username: <input type="text" name="proxy_username"><br>
-        Proxy Password: <input type="password" name="proxy_password"><br>
-        Proxy Exceptions: <input type="text" name="proxy_exceptions"><br>
-        Optional Wifi Static Configuration:
-        <input type="checkbox" name="wifi_static" value="1">IP Address
-        <input type="text" name="wifi_ip"><br>
-        Netmask: <input type="text" name="wifi_netmask"><br>
-        Gateway: <input type="text" name="wifi_gateway"><br>
-        DNS: <input type="text" name="wifi_dns"><br>
-        Secondary DNS: <input type="text" name="wifi_dns_secondary"><br>
-        Optional LAN Static Configuration:
-        <input type="checkbox" name="lan_static" value="1">IP Address
-        <input type="text" name="lan_ip"><br>
-        Netmask: <input type="text" name="lan_netmask"><br>
+
+<html>
+<head>
+<title>SETUP PI WIFI Configuration</title>
+</head>
+<body>
+<h1>SETUP PI WIFI Configuration</h1>
+<form action="wifi.php" method="post">
+<h3>WIFI Credentials</h3>
+SSID: <input type="text" name="ssid"><br>
+Password: <input type="password" name="password"><br>
+Security:
+<input type="radio" name="security" value="wpa"> WPA
+<input type="radio" name="security" value="wpa2"> WPA2
+<input type="radio" name="security" value="wep"> WEP
+<input type="radio" name="security" value="eap"> EAP<br>
+<h3>WIFI Static Configuration (Optional)</h3>
+IP Address: <input type="text" name="wifi_ip"><br>
+Netmask: <input type="text" name="wifi_netmask"><br>
+Gateway: <input type="text" name="wifi_gateway"><br>
+DNS: <input type="text" name="wifi_dns"><br>
+Secondary DNS: <input type="text" name="wifi_dns2"><br>
+<h3>LAN Static Configuration (Optional)</h3>
+IP Address: <input type="text" name="lan_ip"><br>
+Netmask: <input type="text" name="lan_netmask"><br>
+
 Gateway: <input type="text" name="lan_gateway"><br>
 DNS: <input type="text" name="lan_dns"><br>
-Secondary DNS: <input type="text" name="lan_dns_secondary"><br>
+Secondary DNS: <input type="text" name="lan_dns2"><br>
+
+<h3>Proxy Configuration (Optional)</h3>
+Proxy Type:
+<input type="radio" name="proxy_type" value="http"> HTTP
+<input type="radio" name="proxy_type" value="https"> HTTPS<br>
+Proxy Server: <input type="text" name="proxy_server"><br>
+Port: <input type="text" name="proxy_port"><br>
+Username: <input type="text" name="proxy_username"><br>
+Password: <input type="password" name="proxy_password"><br>
+Proxy Exceptions: <input type="text" name="proxy_exceptions"><br>
 <input type="submit" value="Submit">
 </form>
 </body>
-
-  </html>
+</html>
 EOF
+cat > /var/www/html/wifi.php <<EOF
 
-#Step 5: Display GUI on connected devices
-echo "Displaying GUI on connected devices..."
-echo "Connect to the 'SETUP PI' access point to enter your wifi credentials."
-echo "Once connected, open a browser and go to 'setup.pi' to access the GUI."
+<?php
+\$ssid = \$_POST["ssid"];
+\$password = \$_POST["password"];
+\$security = \$_POST["security"];
+\$wifi_ip = \$_POST["wifi_ip"];
+\$wifi_netmask = \$_POST["wifi_netmask"];
+\$wifi_gateway = \$_POST["wifi_gateway"];
+\$wifi_dns = \$_POST["wifi_dns"];
+\$wifi_dns2 = \$_POST["wifi_dns2"];
+\$lan_ip = \$_POST["lan_ip"];
+\$lan_netmask = \$_POST["lan_netmask"];
+\$lan_gateway = \$_POST["lan_gateway"];
+\$lan_dns = \$_POST["lan_dns"];
+\$lan_dns2 = \$_POST["lan_dns2"];
+\$proxy_type = \$_POST["proxy_type"];
+\$proxy_server = \$_POST["proxy_server"];
+\$proxy_port = \$_POST["proxy_port"];
+\$proxy_username = \$_POST["proxy_username"];
+\$proxy_password = \$_POST["proxy_password"];
+\$proxy_exceptions = \$_POST["proxy_exceptions"];
 
-#Step 6: Notify user of successful connection
-echo "Notifying user of successful connection..."
-cat > /var/www/html/submit.php <<EOF
+\$wpa_str = "";
+if (\$security == "wpa") {
+    \$wpa_str = "wpa=1\nwpa_passphrase=\$password\n";
+} elseif (\$security == "wpa2") {
+    \$wpa_str = "wpa=2\nwpa_passphrase=\$password\n";
+} elseif (\$security == "wep") {
+    \$wpa_str = "wep_key0=\$password\n";
+} elseif (\$security == "eap") {
+    \$wpa_str = "ieee8021x=1\nauth_server_addr=127.0.
 
-  <html>
-    <head>
-      <title>SETUP PI Wifi Credentials</title>
-    </head>
-    <body>
-      <?php
-        $ssid = $_POST["ssid"];
-        $password = $_POST["password"];
-        $security = $_POST["security"];
-        $proxy = $_POST["proxy"];
-        $proxy_server = $_POST["proxy_server"];
-        $proxy_port = $_POST["proxy_port"];
-        $proxy_username = $_POST["proxy_username"];
-        $proxy_password = $_POST["proxy_password"];
-        $proxy_exceptions = $_POST["proxy_exceptions"];
-        $wifi_static = $_POST["wifi_static"];
-        $wifi_ip = $_POST["wifi_ip"];
-        $wifi_netmask = $_POST["wifi_netmask"];
-        $wifi_gateway = $_POST["wifi_gateway"];
-        $wifi_dns = $_POST["wifi_dns"];
-        $wifi_dns_secondary = $_POST["wifi_dns_secondary"];
-        $lan_static = $_POST["lan_static"];
-        $lan_ip = $_POST["lan_ip"];
-        $lan_netmask = $_POST["lan_netmask"];
-        $lan_gateway = $_POST["lan_gateway"];
-        $lan_dns = $_POST["lan_dns"];
-        $lan_dns_secondary = $_POST["lan_dns_secondary"];
-		
-    if ($ssid && $password && $security) {
-      // Update /etc/wpa_supplicant/wpa_supplicant.conf with new wifi credentials
-      cp /etc/wpa_supplicant/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant.conf.bak
-      cat > /etc/wpa_supplicant/wpa_supplicant.conf <<EOF
-      network={
-        ssid="$ssid"
-        psk="$password"
-        key_mgmt=$security
-      }
-EOF
 
-      // Update /etc/network/interfaces with optional static wifi configuration
-      if ($wifi_static) {
-        cp /etc/network/interfaces /etc/network/interfaces.bak
-cat > /etc/network/interfaces <<EOF
+0.1\nauth_server_port=1812\nauth_server_shared_secret=testing123\n";
+}
+
+$wifi_static_str = "";
+if ($wifi_ip && $wifi_netmask && $wifi_gateway && $wifi_dns && $wifi_dns2) {
+$wifi_static_str = "static ip_address=$wifi_ip/$wifi_netmask\n gateway=$wifi_gateway\n dns-nameservers $wifi_dns $wifi_dns2\n";
+}
+
+$lan_static_str = "";
+if ($lan_ip && $lan_netmask && $lan_gateway && $lan_dns && $lan_dns2) {
+$lan_static_str = "iface eth0 inet static\n address $lan_ip\n netmask $lan_netmask\n gateway $lan_gateway\n dns-nameservers $lan_dns $lan_dns2\n";
+}
+
+$proxy_str = "";
+if ($proxy_type && $proxy_server && $proxy_port && $proxy_username && $proxy_password && $proxy_exceptions) {
+$proxy_str = "Acquire::$proxy_type::proxy "http://$proxy_username:$proxy_password@$proxy_server:$proxy_port/";\n Acquire::$proxy_type::proxy "https://$proxy_username:$proxy_password@$proxy_server:$proxy_port/";\n Acquire::$proxy_type::proxy::$proxy_exceptions "DIRECT";\n";
+}
+
+$wpa_conf = <<<EOT
+ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
+update_config=1
+
+network={
+ssid="$ssid"
+$wpa_str
+}
+EOT;
+
+file_put_contents("/etc/wpa_supplicant/wpa_supplicant.conf", $wpa_conf);
+
+$interfaces = <<<EOT
+auto lo
+iface lo inet loopback
+
+auto eth0
+$lan_static_str
+
 auto wlan0
-iface wlan0 inet static
-address $wifi_ip
-netmask $wifi_netmask
-gateway $wifi_gateway
-dns-nameservers $wifi_dns $wifi_dns_secondary
-EOF
-} // Update /etc/dhcpcd.conf with optional static LAN configuration
-      if ($lan_static) {
-        cp /etc/dhcpcd.conf /etc/dhcpcd.conf.bak
-        cat > /etc/dhcpcd.conf <<EOF
-        interface eth0
-        static ip_address=$lan_ip/24
-        static routers=$lan_gateway
-        static domain_name_servers=$lan_dns $lan_dns_secondary
-EOF
-      }
+$wifi_static_str
+allow-hotplug wlan0
+iface wlan0 inet dhcp
+wpa-conf /etc/wpa_supplicant/wpa_supplicant.conf
+EOT;
 
-      // Update /etc/environment with optional proxy configuration
-      if ($proxy) {
-        cp /etc/environment /etc/environment.bak
-        cat > /etc/environment <<EOF
-        http_proxy=http://$proxy_server:$proxy_port
-        https_proxy=http://$proxy_server:$proxy_port
-        ftp_proxy=http://$proxy_server:$proxy_port
-        no_proxy="localhost,127.0.0.1,$proxy_exceptions"
-        HTTP_PROXY=http://$proxy_server:$proxy_port
-        HTTPS_PROXY=http://$proxy_server:$proxy_port
-        FTP_PROXY=http://$proxy_server:$proxy_port
-        NO_PROXY="localhost,127.0.0.1,$proxy_exceptions"
+file_put_contents("/etc/network/interfaces", $interfaces);
+
+$apt_conf = <<<EOT
+$proxy_str
+EOT;
+
+file_put_contents("/etc/apt/apt.conf", $apt_conf);
+
+echo "WIFI configuration successfully applied. Please reboot the device to apply changes.";
 EOF
 
-        if ($proxy_username && $proxy_password) {
-          cat >> /etc/environment <<EOF
-          http_proxy=http://$proxy_username:$proxy_password@$proxy_server:$proxy_port/
-          https_proxy=http://$proxy_username:$proxy_password@$proxy_server:$proxy_port/
-          ftp_proxy=http://$proxy_username:$proxy_password@$proxy_server:$proxy_port/
-          HTTP_PROXY=http://$proxy_username:$proxy_password@$proxy_server:$proxy_port/
-          HTTPS_PROXY=http://$proxy_username:$proxy_password@$proxy_server:$proxy_port/
-          FTP_PROXY=http://$proxy_username:$proxy_password@$proxy_server:$proxy_port/
-EOF
-        }
-      }
+#Add script to run on boot
+echo "@reboot /bin/bash /root/setup_pi.
 
-      // Restart networking service to apply changes
-      service networking restart
+sh" >> /etc/crontab
 
-      // Check internet connection
-      if ping -q -c 1 -W 1 google.com >/dev/null; then
-        echo "<h1>Successfully connected to $ssid!</h1>"
-      else
-        echo "<h1>Connection failed. Please try again.</h1>"
-      }
-    } else {
-      echo "<h1>Please enter all required fields.</h1>"
-    }
-  ?>
-</body>
-  </html>
-  EOF
+#Run script after boot
+/bin/bash /root/setup_pi.sh
 
-Step 7: Run script on boot
-echo "Adding script to run on boot..."
-cp /etc/rc.local /etc/rc.local.bak
-cat > /etc/rc.local <<EOF
-#!/bin/bash
+echo "Setup PI configuration complete. Please connect to the 'SETUP PI - Network' access point and navigate to 'setup.pi' to enter your wifi credentials."
 
-Run script on boot
-/path/to/script/raspbian_bullseye_64-bit.sh
 
-exit 0
-EOF
 
-echo "Done. Script will run on boot."
