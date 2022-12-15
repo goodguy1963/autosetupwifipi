f79d0acd645b348a8754af253439bbdecfe722b6
goodguy1963 <49859266+goodguy1963@users.noreply.github.com>
2022-12-15 11:07:34 +0100

    Update accesspoint2.sh

--- accesspoint2.sh
+++ accesspoint2.sh
@@ -1,58 +1,81 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-#Check if internet connection is available
-if [ "$(ping -c 1 8.8.8.8 | grep '100% packet loss')" != "" ]; then
-#Turn on wireless access point with no password
-sudo apt-get install dnsmasq hostapd
-sudo systemctl stop dnsmasq
-sudo systemctl stop hostapd
+# Check if the device has an internet connection
+if ! ping -c 1 -W 1 8.8.8.8; then
+  # If no internet connection is present, set up a new wireless access point
+  # called SETUP PI with no password
+  sudo apt-get update
+  sudo apt-get install dnsmasq hostapd
+  sudo systemctl stop dnsmasq
+  sudo systemctl stop hostapd
 
-#Configure access point settings
-sudo cp /etc/dhcpcd.conf /etc/dhcpcd.conf.bak
-echo "denyinterfaces wlan0" | sudo tee -a /etc/dhcpcd.conf
-sudo systemctl restart dhcpcd
+  # Configure the access point
+  cat <<EOF | sudo tee /etc/hostapd/hostapd.conf
+interface=wlan0
+driver=nl80211
+ssid=SETUP_PI
+hw_mode=g
+channel=7
+wmm_enabled=0
+macaddr_acl=0
+auth_algs=1
+ignore_broadcast_ssid=0
+EOF
 
-#Create access point configuration file
-sudo cp /etc/hostapd/hostapd.conf /etc/hostapd/hostapd.conf.bak
-echo "interface=wlan0" | sudo tee /etc/hostapd/hostapd.conf
-echo "driver=nl80211" | sudo tee -a /etc/hostapd/hostapd.conf
-echo "ssid=SETUP PI" | sudo tee -a /etc/hostapd/hostapd.conf
-echo "hw_mode=g" | sudo tee -a /etc/hostapd/hostapd.conf
-echo "channel=7" | sudo tee -a /etc/hostapd/hostapd.conf
-echo "macaddr_acl=0" | sudo tee -a /etc/hostapd/hostapd.conf
-echo "auth_algs=1" | sudo tee -a /etc/hostapd/hostapd.conf
-echo "ignore_broadcast_ssid=0" | sudo tee -a /etc/hostapd/hostapd.conf
+  # Set the access point as the default in hostapd
+  sudo sed -i 's/#DAEMON_CONF=""/DAEMON_CONF="\/etc\/hostapd\/hostapd.conf"/g' /etc/default/hostapd
 
-#Configure dnsmasq for access point
-sudo cp /etc/dnsmasq.conf /etc/dnsmasq.conf.bak
-echo "interface=wlan0" | sudo tee /etc/dnsmasq.conf
-echo "dhcp-range=192.168.4.2,192.168.4.20,255.255.255.0,24h" | sudo tee -a /etc/dnsmasq.conf
+  # Set up the DHCP server to provide IP addresses to clients
+  cat <<EOF | sudo tee /etc/dnsmasq.conf
+interface=wlan0
+dhcp-range=192.168.4.2,192.168.4.20,255.255.255.0,24h
+EOF
 
-#Enable access point on boot
-sudo systemctl enable hostapd
-sudo systemctl enable dnsmasq
+  # Set up the network interface
+  cat <<EOF | sudo tee /etc/network/interfaces
+source-directory /etc/network/interfaces.d
 
-#Start access point
-sudo systemctl start hostapd
-sudo systemctl start dnsmasq
+auto lo
+iface lo inet loopback
 
-#Create GUI for wifi setup
-sudo apt-get install python3-tk
-python3 -m tkinter -c "from tkinter import ;
-app = Tk();
-app.title('Wifi Setup');
-label1 = Label(app, text='Enter wifi credentials:');
-label1.pack();
-ssid_label = Label(app, text='SSID:');
-ssid_label.pack();
-ssid_entry = Entry(app);
-ssid_entry.pack();
-password_label = Label(app, text='Password:');
-password_label.pack();
-password_entry = Entry(app, show='');
-password_entry.pack();
-def on_click():
-ssid = s
+auto eth0
+iface eth0 inet dhcp
 
+allow-hotplug wlan0
+iface wlan0 inet static
+    address 192.168.4.1
+    netmask 255.255.255.0
+    network 192.168.4.0
+    broadcast 192.168.4.255
+EOF
 
+  # Restart the networking service to apply the changes
+  sudo systemctl restart networking
 
+  # Enable and start the access point services
+  sudo systemctl unmask hostapd
+  sudo systemctl enable hostapd
+  sudo systemctl start hostapd
+  sudo systemctl start dnsmasq
+fi
+
+# Create a GUI where the user can set up real wifi credentials
+# to connect to their own network
+cat <<EOF | sudo tee /usr/local/bin/wifi-setup-gui
+#!/usr/bin/env bash
+
+# Create a GUI to allow the user to set up wifi credentials
+# You can use any GUI toolkit you like for this, such as GTK or QT
+# Here, we will use Zenity as an example
+zenity --forms --title="WiFi Setup" \
+       --text="Enter your wifi credentials" \
+       --separator="," \
+       --add-entry="SSID" \
+       --add-password="Password" > /tmp/wifi-credentials
+
+# Read the wifi credentials from the file
+IFS=","
+read -r ssid password < /tmp/wifi-credentials
+
+# Set up the network interface with the new credentials
+cat <<EOF | sudo tee /etc/w
